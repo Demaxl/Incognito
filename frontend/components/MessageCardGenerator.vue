657efd21b3feb984251fba817c1e9ddd379b5951
Demaxl <aweabdullahi@gmail.com>
2026-07-27 18:12:39 +0100

    Fix tiny text issue in message cards and update package configurations
    - Updated the package.json to set NODE_OPTIONS for the development script.
    - Removed the @netlify/blobs dependency from package-lock.json as it is no longer needed.

--- frontend/components/MessageCardGenerator.vue
+++ frontend/components/MessageCardGenerator.vue
@@ -1,7 +1,5 @@
 <template>
-    <div
-        ref="cardContainer"
-        style="
+    <div ref="cardContainer" style="
             position: absolute;
             left: -99999px;
             top: 0;
@@ -9,8 +7,7 @@
             height: 1920px;
             overflow: hidden;
         "
-        class="rounded-lg bg-gradient-to-r relative py-[200px] to-primary-500 from-[rgb(0,255,143)] text-white flex flex-col"
-    >
+        class="rounded-lg bg-gradient-to-r relative py-[200px] to-primary-500 from-[rgb(0,255,143)] text-white flex flex-col">
         <!-- <div
         ref="cardContainer"
         class="rounded-lg bg-gradient-to-r relative to-primary-500 from-[rgb(0,255,143)] p-6 text-white flex flex-col"
@@ -23,98 +20,54 @@
         <div class="absolute bottom-0 right-0 opacity-50 rotate-180">
             <IconLucideQuote />
         </div>
-        <div
-            class="absolute top-12 right-12 w-20 h-20 rounded-full bg-white/30"
-        ></div>
-        <div
-            class="absolute bottom-12 left-12 w-32 h-32 rounded-full bg-white/30"
-        ></div>
+        <div class="absolute top-12 right-12 w-20 h-20 rounded-full bg-white/30"></div>
+        <div class="absolute bottom-12 left-12 w-32 h-32 rounded-full bg-white/30"></div>
 
         <!-- Message Content -->
         <div class="flex-1 flex flex-col items-center p-6 justify-center">
             <!-- Text Message -->
-            <p
-                v-if="message.message_type === 'text'"
-                class="text-center text-6xl font-light leading-relaxed"
-            >
+            <p v-if="message.message_type === 'text'" class="text-center font-light"
+                style="font-size: 72px; line-height: 1.4">
                 {{ message.text }}
             </p>
 
             <!-- Image Message -->
-            <div
-                v-else-if="message.message_type === 'image'"
-                class="flex flex-col items-center gap-4"
-            >
-                <p
-                    v-if="message.text"
-                    class="text-center text-4xl font-light leading-relaxed"
-                >
+            <div v-else-if="message.message_type === 'image'" class="flex flex-col items-center gap-4">
+                <p v-if="message.text" class="text-center font-light" style="font-size: 48px; line-height: 1.4">
                     {{ message.text }}
                 </p>
                 <div class="relative w-[800px] h-[800px] bg-transparent">
-                    <img
-                        :src="message.content"
-                        :alt="message.text || 'Image message'"
-                        :data-message-id="message.id"
-                        class="w-full h-full object-contain rounded-lg"
-                        @load="onImageLoad"
-                        ref="messageImage"
-                    />
+                    <img :src="message.content" :alt="message.text || 'Image message'" :data-message-id="message.id"
+                        class="w-full h-full object-contain rounded-lg" @load="onImageLoad" ref="messageImage" />
                 </div>
             </div>
 
             <!-- Video Message -->
-            <div
-                v-else-if="message.message_type === 'video'"
-                class="flex flex-col items-center gap-4"
-            >
-                <p
-                    v-if="message.text"
-                    class="text-center text-4xl font-light leading-relaxed"
-                >
+            <div v-else-if="message.message_type === 'video'" class="flex flex-col items-center gap-4">
+                <p v-if="message.text" class="text-center font-light" style="font-size: 48px; line-height: 1.4">
                     {{ message.text }}
                 </p>
-                <div
-                    v-else
-                    class="text-center text-4xl font-light leading-relaxed"
-                >
+                <div v-else class="text-center font-light" style="font-size: 48px; line-height: 1.4">
                     Video Message
                 </div>
-                <div
-                    class="relative w-[800px] h-[400px] bg-transparent flex items-center justify-center"
-                >
+                <div class="relative w-[800px] h-[400px] bg-transparent flex items-center justify-center">
                     <!-- Video thumbnail with play button overlay -->
-                    <div
-                        class="w-full h-full flex items-center justify-center rounded-lg"
-                    >
+                    <div class="w-full h-full flex items-center justify-center rounded-lg">
                         <IconLucideVideo class="h-32 w-32 fill-white" />
                     </div>
                 </div>
             </div>
 
             <!-- Audio Message -->
-            <div
-                v-else-if="message.message_type === 'audio'"
-                class="flex flex-col items-center gap-4"
-            >
-                <p
-                    v-if="message.text"
-                    class="text-center text-4xl font-light leading-relaxed"
-                >
+            <div v-else-if="message.message_type === 'audio'" class="flex flex-col items-center gap-4">
+                <p v-if="message.text" class="text-center font-light" style="font-size: 48px; line-height: 1.4">
                     {{ message.text }}
                 </p>
-                <div
-                    v-else
-                    class="text-center text-4xl font-light leading-relaxed"
-                >
+                <div v-else class="text-center font-light" style="font-size: 48px; line-height: 1.4">
                     Audio Message
                 </div>
-                <div
-                    class="relative w-[800px] h-[200px] bg-transparent flex items-center justify-center"
-                >
-                    <div
-                        class="w-full h-full flex items-center justify-center rounded-lg"
-                    >
+                <div class="relative w-[800px] h-[200px] bg-transparent flex items-center justify-center">
+                    <div class="w-full h-full flex items-center justify-center rounded-lg">
                         <IconLucideAudio class="h-32 w-32 fill-white" />
                     </div>
                 </div>
@@ -122,20 +75,18 @@
         </div>
         <div class="mt-auto pt-6">
             <div class="w-[50%] h-1 bg-white/90 mx-auto mb-4"></div>
-            <p class="text-center opacity-90 text-5xl font-light">
+            <p class="text-center opacity-90 font-light" style="font-size: 52px">
                 @{{ username }}
             </p>
         </div>
         <div class="mt-32 flex flex-col items-center">
             <div class="mb-2 rounded-full p-6 flex items-center justify-center">
-                <img
-                    src="/images/logo_icon_colored.png"
-                    alt="Incognito logo"
-                    class="w-[100px] h-[100px]"
-                />
+                <img src="/images/logo_icon_colored.png" alt="Incognito logo" class="w-[100px] h-[100px]" />
             </div>
             <!-- <UIcon name="i-custom-logo-text" size="40px" class="text-white" /> -->
-            <p class="text-4xl font-bold tracking-wider">#INCOGNITO</p>
+            <p class="font-bold tracking-wider" style="font-size: 40px">
+                #INCOGNITO
+            </p>
             <!-- <UIcon name="i-custom-logo" size="40" /> -->
         </div>
     </div>
